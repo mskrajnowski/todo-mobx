e9a2f1c4f0edd4e117374caedf4dd56c4f7d9d6b
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2019-01-12 21:17:42 +0100

    Added a SCSS module to the default component

--- generators/component.js
+++ generators/component.js
@@ -15,5 +15,10 @@ module.exports = {
       path: "src/components/{{pascalCase name}}.tsx",
       templateFile: path.join(__dirname, "component.tsx.hbs"),
     },
+    {
+      type: "add",
+      path: "src/components/{{pascalCase name}}.module.scss",
+      templateFile: path.join(__dirname, "component.module.scss.hbs"),
+    },
   ],
 }
