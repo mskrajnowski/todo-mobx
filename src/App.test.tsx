5b8232f0f659a35143afd2e930cb641e22a103f8
Marek Skrajnowski <mskrajnowski@users.noreply.github.com>
2019-01-13 22:58:37 +0100

    Fix: minimal TodosStore mock

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom"
+
 import App from "./App"
+import { container } from "./di"
+import { TodosStore } from "./services/TodosStore"
 
 it("renders without crashing", () => {
+  container.bind(TodosStore).toConstantValue(({
+    active: [],
+    archived: [],
+  } as any) as TodosStore)
+
   const div = document.createElement("div")
   ReactDOM.render(<App />, div)
   ReactDOM.unmountComponentAtNode(div)
